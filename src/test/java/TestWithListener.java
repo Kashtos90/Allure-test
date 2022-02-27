9d8caa75ab06b5b941b57946b7f1aa9ed292a7ba
Kashtos90 <29061990a@gmail.com>
2022-02-27 15:37:16 +0700

    added TestWithListener

---
+++ src/test/java/TestWithListener.java
@@ -0,0 +1,31 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestWithListener {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    public void testIssueSearch(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("Kashtos90/Allure-test");
+        $(".header-search-input").pressEnter();
+        $(new By.ByPartialLinkText("Issues")).should(Condition.exist);
+    }
+
+}
